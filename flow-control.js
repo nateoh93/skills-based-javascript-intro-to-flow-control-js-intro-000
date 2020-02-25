4b3ccb5b8f3ce66ef6d64b27a2e31e775eee1628
Nathan Oh <nathanoh93@gmail.com>
2020-02-25 21:28:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,14 +1,14 @@
 function basicTeenager(age) {
 if (age >= 13 && age <= 19) {
   return "You are a teenager!"
-} 
+}
 }
 
 function teenager(age) {
-  if (`${age}`> 12 && `${age}` < 20) {
-    return `You are a teenager!`
+  if (age > 12 && age < 20) {
+    return "You are a teenager!"
   } else {
-    return `You are not a teenager`
+    return "You are not a teenager"
   }
 
 }
